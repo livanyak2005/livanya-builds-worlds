f51a6c5ad33c9a4cf2d7bf11f79504083d46f8f0
Livanya K <livanyak2005@gmail.com>
2026-08-26 00:13:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  base: "/livanya-builds-worlds/",
+  base: '/livanya-builds-worlds/',
 
   tanstackStart: {
     server: { entry: "server" },
